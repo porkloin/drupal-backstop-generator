d3205b07e362a1c99e7eab3e51342f1dbbaaff79
Ryan Bateman <rybateman@gmail.com>
2018-01-03 08:24:39 -0900

    adding bin for global command

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ const validUrl = require('valid-url');
 const cli = meow(
     `
     Usage
-      $ backstop-generator <infile.json>
-    Options
-      --outfile, -o        Save the backstop config to this file.
-      --paths, -p          Additional paths to pass into the configuration file.
+      $ drupal-backstop-generator <infile.json>
     Examples
-      $ backstop-crawl mypageurls.json
+      $ drupal-backstop-generator http://my.drupal.local/all-content-json
 `,
     {
         alias: {
@@ -31,14 +28,6 @@ if (cli.input.length > 0) {
   } else {
     console.error('> Not a valid URL');
   }
-  //if (fs.existsSync(cli.input[0])) {
-  //parse(cli.input[0]);
-  //}
-  /* else {
-        console.error(`> Error: "${cli.input[0]}" doesn't exist.`);
-        process.exit(1);
-    }
-    */
 } else {
     cli.showHelp();
 }
